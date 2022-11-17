02dc6fdfee24e7cea5f71d6fd36d709caf97c3b0
MrPandey2k <66745368+MrPandey2k@users.noreply.github.com>
2022-11-17 11:32:43 -0500

    basic navigation bar

---
+++ OurFavoriteMusicGenres/Controllers/GenreController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OurFavoriteMusicGenres.Controllers
+{
+    public class GenreController: Controller
+    {
+
+        private readonly ILogger<GenreController> _logger;
+
+        public GenreController(ILogger<GenreController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Rap()
+        {
+            return View();
+        }
+
+    }
+}
