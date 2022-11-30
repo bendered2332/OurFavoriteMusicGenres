eb57174cdd5e2fd5a7e9a32072f1ee134c3ef4e5
schottlw <schottlw@mail.uc.edu>
2022-11-29 19:35:29 -0500

    Added pre-populated data to SongContext

--- OurFavoriteMusicGenres/Models/SongContext.cs
+++ OurFavoriteMusicGenres/Models/SongContext.cs
@@ -10,5 +10,51 @@ namespace OurFavoriteMusicGenres.Models
 
         public DbSet<Song> Songs { get; set; }
         public DbSet<Genre> Genres { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Song>().HasData(
+                new Song
+                {
+                    Title = "Karma",
+                    Artist = "Taylor Swift",
+                    GenreId = "P"
+                },
+                new Song
+                {
+                    Title = "Pink Venom",
+                    Artist = "BLACKPINK",
+                    GenreId = "P"
+                }
+            );
+
+            modelBuilder.Entity<Genre>().HasData(
+                new Genre
+                {
+                    GenreId = "IF",
+                    Name = "Indie Folk"
+                },
+                new Genre
+                {
+                    GenreId = "IR",
+                    Name = "Indie Rock"
+                },
+                new Genre
+                {
+                    GenreId = "P",
+                    Name = "Pop"
+                },
+                new Genre
+                {
+                    GenreId = "R",
+                    Name = "Rap"
+                },
+                new Genre
+                {
+                    GenreId = "H",
+                    Name = "Hip Hop"
+                }
+            );
+        }
     }
 }
